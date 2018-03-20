2209aff0911720ddd6f2b152839968fd4f42fa9d
caiyi <caiyiyonyou.com>
2018-03-20 11:23:11 +0800

    图片多选

--- js/userFeedback.js
+++ js/userFeedback.js
@@ -1,4 +1,4 @@
-var g_pathArr = [];
+var g_path = [];
 
 summerready = function() {
 	bindEvents();
@@ -19,21 +19,67 @@ function bindEvents() {
 
 function closePic(obj) {
 	var src = $(obj).prev("img").attr("src");
-	g_pathArr.remove(src);
+	g_path.remove(src);
 	$(obj).parent().parent(".con").remove();
 	var len = $("#ibox .con").length - 1;
 	$('#picShow').text(len + '/4');
 	$("#plus").removeClass("none");
 }
 
-function camera() {
-	//打开相机
-	summer.openCamera({
-		compressionRatio : 0.2,
-		callback : function(ret) {
-			var imgPath = ret.compressImgPath;
-			g_pathArr.push(imgPath);
-			var picDiv = '<div class="con">' + '<div class="con-con">' + '<img src="' + imgPath + '"><div class="bottomDelete"></div>' + '<span class="close iconfont icon-backspace" onClick="closePic(this);"></span>' + '</div></div>';
+function showActionsheet() {
+	UM.actionsheet({
+		title : '',
+		items : ['拍照', '从相册中选择'],
+		callbacks : [
+		function() {
+			// 打开相机
+			summer.openCamera({
+				bindfield : "image",
+				callback : function(args) {
+					var path = args.imgPath;
+					compressImg(path);
+				}
+			})
+		},
+		function() {
+			// 打开相册
+			var count = 4 - parseInt(g_path.length);
+			summer.openPhotoAlbum({
+				bindfield : "image",
+				maxCount : count,
+				type : "multiple", //支持选多张图片
+				callback : function(args) {
+					var paths = args.imgPaths;
+					for (var i = 0; i < paths.length; i++) {
+						compressImg(paths[i].imgPath);
+					}
+				}
+			});
+		}]
+
+	});
+}
+
+/*压缩图片*/
+function compressImg(path) {
+	var pathArr = path.split('/');
+	var newPath = pathArr[pathArr.length - 1];
+	// 调用上传
+	summer.compressImg({
+		src : path,
+		path : 'compressImg/camera' + newPath,
+		quality : "0.2", // 质量压缩比例
+		callback : function(arg) {
+			if (g_path.length >= 4) {
+				return
+			}
+			var id = String(g_path.length);
+			var picObj = {
+				"path" : arg.savepath,
+				"jid" : id
+			};
+			g_path.push(picObj);
+			var picDiv = '<div class="con">' + '<div class="con-con">' + '<img src="' + arg.savepath + '"><div class="bottomDelete"></div>' + '<span class="close iconfont icon-backspace" onClick="closePic(this);"></span>' + '</div></div>';
 			$("#plus").before(picDiv);
 			var len = $("#ibox .con").length - 1;
 			$('#picShow').text(len + '/4');
@@ -44,49 +90,6 @@ function camera() {
 	})
 }
 
-function openPhotoAlbum() {
-	//打开相册
-	//maxCount是和type: "multiple"  一块使用的
-	var count = 4;
-	summer.openPhotoAlbum({
-		compressionRatio : 0.2,
-		maxCount : count,
-		type : "multiple", //支持选多张图片
-		callback : function(ret) {
-			var imgPath = ret.compressImgPath;
-			g_pathArr.push(imgPath);
-			var picDiv = '<div class="con">' + '<div class="con-con">' + '<img src="' + imgPath + '"><div class="bottomDelete"></div>' + '<span class="close iconfont icon-backspace" onClick="closePic(this);"></span>' + '</div></div>';
-			$("#plus").before(picDiv);
-			var len = $("#ibox .con").length - 1;
-			$('#picShow').text(len + '/4');
-			if (len == 4) {
-				$("#plus").addClass("none");
-			}
-		}
-	});
-}
-
-Array.prototype.indexOf = function(val) {
-	for (var i = 0; i < this.length; i++) {
-		if (this[i] == val)
-			return i;
-	}
-	return -1;
-};
-Array.prototype.remove = function(val) {
-	var index = this.indexOf(val);
-	if (index > -1) {
-		this.splice(index, 1);
-	}
-};
-
-function showActionsheet() {
-	UM.actionsheet({
-		title : '',
-		items : ['拍照', '从相册中选择'],
-		callbacks : [camera, openPhotoAlbum]
-	});
-}
 
 function uploadQuestion() {
 	var question = $.trim($("#question").val());
@@ -104,33 +107,26 @@ function manyfileupload() {
 	}
 	summer.showProgress();
 	// 判断变量
-	var photoPath;
-	var photoList = [];
-	if (g_pathArr.length == 0) {
-		photoList = [];
-	} else {
-		for (var i = 0,
-		    length = g_pathArr.length; i < length; i++) {
-			var photoPath = {
-				"fileURL" : "",
-				"type" : "image/png",
-				"name" : "file"
-			};
-			photoPath.fileURL = g_pathArr[i];
-			photoList.push(photoPath);
+	var fileArray = [];
+	for (var i = 0; i < g_path.length; i++) {
+		var item = {
+			fileURL : g_path[i].path,
+			type : "image/jpeg",
+			name : "file" + g_path[i].jid
 		}
+		fileArray.push(item);
 	}
 	var userinfo = summer.getStorage("userinfo");
-	var SERVER = 'http://10.6.236.6:8080/fileUpload/servlet/UploadHandleServlet';
+	var SERVER = G_COMMON_URL+'/advice/addAdvice';
 
 	var params = {
 		context : $.trim($("#question").val())
 	};
-	var header = getDeviceidAndToken();
+	var headers = getDeviceidAndToken();
 	summer.multiUpload({
-		fileArray : photoList,
+		fileArray : fileArray,
 		params : params,
-		headers : header,
+		headers : headers,
 		SERVER : SERVER
 	}, function(ret) {
 
